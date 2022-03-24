06ebc5164a2f9958f6f245ed9d51626d980095ea
Nikhil Paleti <paletinikhil@gmail.com>
2022-03-25 00:48:33 +0530

    Update hits.py
    And (hopefully) final/last Synopsis iteration

--- hits.py
+++ hits.py
@@ -6,8 +6,25 @@ class Node:
         self.auth = 1.0
         self.hub = 1.0
 
-    def update_auth(self):
-       self.auth = sum(node.hub for node in self.parents)
+    def HITS_one_iter(graph):
+        node_list = graph.nodes
 
+        for node in node_list:
+            node.update_auth()
+        for node in node_list:
+            node.update_hub()
+
+        graph.normalize_auth_hub()
+
+    def update_auth(self):
+        self.auth = sum(node.hub for node in self.parents)
     def update_hub(self):
         self.hub = sum(node.auth for node in self.children)
+ 
+    def normalize_auth_hub(self):
+        auth_sum = sum(node.auth for node in self.nodes)
+        hub_sum = sum(node.hub for node in self.nodes)
+
+        for node in self.nodes:
+            node.auth /= auth_sum
+            node.hub /= hub_sum
